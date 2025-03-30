29e66217f4a9f3e600d19bc9ddf2d2fcb9d6d905
Sasmitha Kavindu <73338956+sasmithaK@users.noreply.github.com>
2025-03-30 12:09:49 +0530

    feat: delivery management service

---
+++ backend/devlivery-management-service/src/main/java/com/fooddeliverysystem/devliverymanagementservice/DevliveryManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fooddeliverysystem.devliverymanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevliveryManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevliveryManagementServiceApplication.class, args);
+    }
+
+}
---
+++ backend/devlivery-management-service/src/main/java/com/fooddeliverysystem/devliverymanagementservice/dto/OrderRequest.java
@@ -0,0 +1,15 @@
+package com.fooddeliverysystem.devliverymanagementservice.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderRequest {
+    private List<OrderLineItemDto> orderLineItemsDtoList;
+}
