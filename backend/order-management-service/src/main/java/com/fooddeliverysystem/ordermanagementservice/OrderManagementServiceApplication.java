3c225f93454ef4158a391061f100c64bd243eec2
Sasmitha Kavindu <73338956+sasmithaK@users.noreply.github.com>
2025-03-30 12:07:12 +0530

    feat update: order management service

--- backend/order-management-service/src/main/java/com/fooddeliverysystem/ordermanagementservice/OrderManagementServiceApplication.java
+++ backend/order-management-service/src/main/java/com/fooddeliverysystem/ordermanagementservice/OrderManagementServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NewServiceApplication {
+public class OrderManagementServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NewServiceApplication.class, args);
+        SpringApplication.run(OrderManagementServiceApplication.class, args);
     }
 
 }
--- backend/order-management-service/src/test/java/com/fooddeliverysystem/ordermanagementservice/OrderManagementServiceApplicationTests.java
+++ backend/order-management-service/src/test/java/com/fooddeliverysystem/ordermanagementservice/OrderManagementServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NewServiceApplicationTests {
+class OrderManagementServiceApplicationTests {
 
     @Test
     void contextLoads() {
