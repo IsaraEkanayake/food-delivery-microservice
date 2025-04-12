722edce5c1e854f028ff75c09d6ccf5c363909d3
Sasmitha Kavindu <73338956+sasmithaK@users.noreply.github.com>
2025-04-12 20:38:31 +0530

    chore: modified file structure, added Dockerfile and k8s for orchestration

---
+++ delivery-management-service/src/main/java/com/fooddeliverysystem/devliverymanagementservice/DeliveryManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fooddeliverysystem.devliverymanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryManagementServiceApplication.class, args);
+    }
+
+}
